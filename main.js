f4c4d6bb292c788062fa3f96e00a1199396f5eab
PeterGill800 <gillp064.319@lgflmail.net>
2018-05-03 18:12:59 +0100

    added files

---
+++ main.js
@@ -0,0 +1,18 @@
+//init canvas
+var canvas = document.getElementById("game");
+var ctx = canvas.getContext("2d");
+
+var canvasColour = "#d1f3ff";
+
+var card = {
+	x: 300,
+	y: 500,
+	imageSize: {
+		x: 53,
+		y: 91,
+	}
+};
+card.picture1 = new Image();
+card.picture1.src = "./assets/card1.png";
+card.currentPicture = card.picture1;
+setTimeout(function(){ctx.drawImage(card.currentPicture, 0, 0, card.imageSize.x, card.imageSize.y, card.x - card.imageSize.x / 2, card.y - card.imageSize.y / 2, card.imageSize.x, card.imageSize.y);},3000);
