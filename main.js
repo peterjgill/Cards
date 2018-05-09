c20dc244439a64297f4ca39408b9ced6831c1308
PeterGill800 <gillp064.319@lgflmail.net>
2018-05-09 20:51:54 +0100

    Fixed bug that broke game after first round

--- main.js
+++ main.js
@@ -9,57 +9,53 @@ var leading = [];
 var aiii = 0;
 var lowest = 15;
 var selected = 0;
+var winners = [];
+var possible = 0;
+var suits = [];
+var values = [];
+var deck = [];
+var shuffleDeck = [];
+var hands = [];
+var roundNumber = 0;
+var enter = 1;
 
 ctx.mozImageSmoothingEnabled = false;
 ctx.webkitImageSmoothingEnabled = false;
 ctx.msImageSmoothingEnabled = false;
 ctx.imageSmoothingEnabled = false;
 
-var cardback = {
-	x: 300,
-	y: 500,
-	imageSize: {
-		x: 68,
-		y: 103,
-	}
-}
-cardback.picture1 = new Image();
-cardback.picture1.src = "./assets/cardback.png";
-cardback.currentPicture = cardback.picture1;
-
-var suits = ["Diamonds","Hearts","Spades","Clubs"];
-var values = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
-var deck = [];
-var shuffleDeck = [];
-var hands = [];
-
-for (var i = 0; i < 4; i++){
-	for (var x = 0; x < 13; x++){
-		var y = new makeCard(suits[i],values[x]);
-		deck.push(y);
-	}
-}
-
-for (var i = 0; i < 52; i++){
-	shuffle();
-}
-
 var players = 0;
 while(players < 2 || players > 7 || players != parseInt(players, 10))
 players = prompt("Please enter number of players between 2 and 7:");
 var who = 0;
 
-for (var i = 0; i < players; i++){
-	hands.push([]);
-}
-
-for (i = 0; i < 7; i++){
-	for (x = 0; x < players; x++){
-		hands[x].push(shuffleDeck[0]);
-		shuffleDeck.splice(0,1);
+makeDeck();
+function makeDeck(){
+	suits = ["Diamonds","Hearts","Spades","Clubs"];
+	values = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+	deck = [];
+	shuffleDeck = [];
+	hands = [];
+	for (var i = 0; i < 4; i++){
+		for (var x = 0; x < 13; x++){
+			var y = new makeCard(suits[i],values[x]);
+			deck.push(y);
+		}
 	}
+	for (var i = 0; i < 52; i++){
+		shuffle();
+	}
+	for (var i = 0; i < players; i++){
+		hands.push([]);
+	}	
+	for (i = 0; i < 7-roundNumber; i++){
+		for (x = 0; x < players; x++){
+			hands[x].push(shuffleDeck[0]);
+			shuffleDeck.splice(0,1);
+		}
+	}
+	render();
 }
-render();
 
 document.addEventListener('keydown', function(event){
 		if(event.keyCode == 40){
@@ -74,7 +70,9 @@ document.addEventListener('keydown', function(event){
 				selected = hands[0].length-1;
 			}
 		}
-		if(event.keyCode == 13 && (play.length == players || play.length == 0)){
+		if(event.keyCode == 13 && enter == 1 &&(play.length == players || play.length == 0)){
+			enter = 0;
+			console.log(players);
 			if(leading.length == 0){
 				leading.push(hands[0][selected]);
 				play.splice(0,1,hands[0][selected]);
@@ -99,6 +97,7 @@ document.addEventListener('keydown', function(event){
 		render();
 	}
 );
+
 var first = [];
 var trump = [];
 function winner(){
@@ -185,24 +184,72 @@ function check(type){
 		for(var i = 0; i < play.length; i ++){
 			if(type[index] == play[i]){
 				who = i;
+				winners.push(who);
 				play = [];
 				trump = [];
 				first = [];
 				highest = 0;
 				leading = [];
-				for(x = 0; x < who ; x++){
+				for(var x = 0; x < who ; x++){
 					play.push([]);
 				}
-				console.log(who);
 				time = who;
 				ctx.fillStyle="blue";
 				ctx.fillRect(280, 40+(20*i),10,10);
 				ctx.font = "18px Arial";
 				ctx.fillText("Press any key to continue", 250, 250);
-				aiii = 1;
+				console.log(winners);
+				if(winners.length == 7 - roundNumber){
+					round();
+				}
+				else{
+					aiii = 1;
+				}
 			}
 		}
 	},500);
+}
+
+function round(){
+	for(var i = 0; i < winners.length; i++){
+		if(winners[i] == 0){
+			possibleCard.push(winners[i]);
+			console.log("p "+possibleCard);
+		}
+		else{
+			console.log(winners[i]);
+			for(var x = 0; x < imimpossibleCard.length; x++){
+				if(winners[i] == imimpossibleCard[x]){
+					impossibleCard.push(winners[i]);
+					console.log("im "+impossibleCard);
+				}
+			}
+			if(impossibleCard.length == 0){
+				imimpossibleCard.push(winners[i]);
+				console.log("imim "+imimpossibleCard);
+			}
+			impossibleCard = [];
+			console.log("im "+impossibleCard);
+		}
+	}
+	if(possibleCard.length == 0){
+		possible = 1;
+		ctx.fillStyle = "black";
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+		ctx.fillStyle = "white";
+		ctx.font = "72px Arial";
+		ctx.fillText("Game Over", 100, 300);
+	}
+	else{
+		players = imimpossibleCard.length + 1;
+		imimpossibleCard = [];
+		possibleCard = [];
+		roundNumber++;
+		enter = 1;
+		play = [];
+		winners = [];
+		makeDeck();
+	}
 }
 
 function aii(){
@@ -224,7 +271,13 @@ function aii(){
 			if(time < players){
 				aii();
 			}
+			else{
+				enter = 1;
+			}
 		}, 500);
+	}
+	else{
+		enter = 1;
 	}
 }
 
@@ -235,7 +288,6 @@ function ai() {
 			play.splice(time, 1, hands[time][x]);
 			hands[time].splice(x,1);
 			time++;
-			console.log(play);
 			render();
 			if(time < who || (who == 0 && time < players)) {
 				ai(); 
@@ -423,41 +475,43 @@ function chooseCard(hand){
 }
 
 function render(){
-	ctx.fillStyle = canvasColour;
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	ctx.fillStyle="blue";
-	ctx.fillRect(10, 40+(20*selected),10,10);
-	for(var i = 0; i < hands[0].length; i++){
-		if (hands[0][i].suit == "Hearts" || hands[0][i].suit == "Diamonds"){
-			ctx.fillStyle = "red";
-		}
-		else{
-			ctx.fillStyle = "black";
-		}
-		ctx.font = "18px Arial";
-		ctx.fillText(""+hands[0][i].value+" of "+hands[0][i].suit, 30, 50+(20*i));
-	}
-	for(var i = 0; i < play.length; i++){
-		if(play[i].suit != undefined){
-			if(play[i].suit == "Hearts" || play[i].suit == "Diamonds"){
+	if(possible == 0){
+		ctx.fillStyle = canvasColour;
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+		ctx.fillStyle="blue";
+		ctx.fillRect(10, 40+(20*selected),10,10);
+		for(var i = 0; i < hands[0].length; i++){
+			if (hands[0][i].suit == "Hearts" || hands[0][i].suit == "Diamonds"){
 				ctx.fillStyle = "red";
 			}
 			else{
 				ctx.fillStyle = "black";
 			}
 			ctx.font = "18px Arial";
-			ctx.fillText(""+play[i].value+" of "+play[i].suit, 300, 50+(20*i));
+			ctx.fillText(""+hands[0][i].value+" of "+hands[0][i].suit, 30, 50+(20*i));
 		}
+		for(var i = 0; i < play.length; i++){
+			if(play[i].suit != undefined){
+				if(play[i].suit == "Hearts" || play[i].suit == "Diamonds"){
+					ctx.fillStyle = "red";
+				}
+				else{
+					ctx.fillStyle = "black";
+				}
+				ctx.font = "18px Arial";
+				ctx.fillText(""+play[i].value+" of "+play[i].suit, 300, 50+(20*i));
+			}
+		}
+		if (shuffleDeck[0].suit == "Hearts" || shuffleDeck[0].suit == "Diamonds"){
+			ctx.fillStyle = "red";
+		}
+		else{
+			ctx.fillStyle = "black";
+		}
+		ctx.font = "18px Arial";
+		ctx.fillText("Trump suit:", 30, 550);
+		ctx.fillText(""+shuffleDeck[0].suit, 30, 570);
 	}
-	if (shuffleDeck[0].suit == "Hearts" || shuffleDeck[0].suit == "Diamonds"){
-		ctx.fillStyle = "red";
-	}
-	else{
-		ctx.fillStyle = "black";
-	}
-	ctx.font = "18px Arial";
-	ctx.fillText("Trump suit:", 30, 550);
-	ctx.fillText(""+shuffleDeck[0].suit, 30, 570);
 }
 
 function makeCard(suit, value) {
